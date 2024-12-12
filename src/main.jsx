4e9b57b6afdde31f8c61299eb5b5e2988ed62291
duongkhanhb1k39 <duongkhanhb1k39@gmail.com>
2024-12-12 23:58:46 +0700

    config prj structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
-import { render } from 'preact'
-import './index.css'
-import { App } from './app.jsx'
-
-render(<App />, document.getElementById('app'))
+import { render } from "preact";
+import "./index.css";
+import { App } from "./app.jsx";
+import "antd/dist/reset.css";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
